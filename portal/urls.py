7b27f40cfc27c69d719bf0e5df0a8f7f21de6eca
celso <celsobadu@hotmail.com>
2020-01-21 16:24:25 -0300

    celso-master

--- portal/urls.py
+++ portal/urls.py
@@ -9,12 +9,12 @@ urlpatterns = [
 	path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 	path('post/<pk>/remove/', views.post_remove, name='post_remove'),
 	#patch da noticia
-	path('', views.post_list_noticia, name='post_list_noticia'),
+	path('post_list_noticia', views.post_list_noticia, name='post_list_noticia'),
 	path('post/noticia', views.post_noticia, name='post_noticia'),
 	path('post/<int:pk>/detail_noticia', views.post_detail_noticia, name='post_detail_noticia'),
 	path('post/<int:pk>/edit_noticia/', views.post_edit_noticia, name='post_edit_noticia'),
 	path('post/<pk>/remove_noticia/', views.post_remove_noticia, name='post_remove_noticia'),
 	path('post/<int:pk>/publicar_noticia/', views.post_publicar_noticia, name='post_publicar_noticia'),
 	#path do visitante
-	path('post_visitante', views.post_visitante, name='post_visitante'),
+	path('', views.post_visitante, name='post_visitante'),
 ]
